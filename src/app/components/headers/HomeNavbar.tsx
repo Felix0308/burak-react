4bc41541dba5c76348f9ebde22d1069c7f1865a7
Felix0308 <utkirbekm.0327@gmail.com>
2025-01-16 20:24:31 +0900

    feat: integration footer and basket source

--- src/app/components/headers/HomeNavbar.tsx
+++ src/app/components/headers/HomeNavbar.tsx
@@ -1,8 +1,8 @@
 import { Box, Button, Container, Stack } from "@mui/material";
-import React from "react";
 import { NavLink } from "react-router-dom";
+import Basket from "./Basket";
 
-export function HomeNavbar() {
+export default function HomeNavbar() {
   const authMember = null;
   return (
     <div className="home-navbar">
@@ -43,7 +43,7 @@ export function HomeNavbar() {
                 Help
               </NavLink>
             </Box>
-            {/* BASKET */}
+            <Basket />
 
             {!authMember ? (
               <Box>
