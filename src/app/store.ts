e5041d73aea281e6b6fa76d5730a07e34cd559b1
Felix0308 <utkirbekm.0327@gmail.com>
2025-02-11 11:51:56 +0900

    feat: middleware integration redux logger

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,7 +1,12 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import { configureStore, ThunkAction, Action, } from '@reduxjs/toolkit';
 import HomePageReducer from './screens/homePage/slice';
+import reduxLogger from "redux-logger";
 
 export const store = configureStore({
+  middleware: (getDefaultMiddleware) =>
+    // @ts-ignore
+    getDefaultMiddleware().concat(reduxLogger), // reduxLogger integratsiaysisni amalga oshirdik
+  // reduxLogger => redux Storage da qanday malumotlar mavjud va qanday o'zgarayotganaini login qilib beradi
   reducer: {
     homePage: HomePageReducer, // Redux Storage ga HomePage screen componentimizga daxldor bo'lgan Slice ichidagi reducerni bog'ladik
   },
