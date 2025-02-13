decb5359ad040526cd10054e896b8e3a7315f28d
Felix0308 <utkirbekm.0327@gmail.com>
2025-02-13 19:36:24 +0900

    fix: retrieve fresh menu and modify NewDishes component

--- src/app/screens/homePage/PopularDishes.tsx
+++ src/app/screens/homePage/PopularDishes.tsx
@@ -16,7 +16,6 @@ import { Product } from "../../../lib/types/product";
 import { serverApi } from "../../../lib/config";
 
 /** REDUX SLICE & SELECTOR **/
-
 const popularDishesRetriever = createSelector(
   retrievePopularDishes,
   (popularDishes) => ({ popularDishes })
@@ -25,8 +24,6 @@ const popularDishesRetriever = createSelector(
 export default function PopularDishes() {
   const { popularDishes } = useSelector(popularDishesRetriever);
 
-  console.log("popularDishes:", popularDishes);
-  
   return (
     <div className="popular-dishes-frame">
       <Container>
@@ -34,10 +31,10 @@ export default function PopularDishes() {
           <Box className="category-title">Popular Dishes</Box>
           <Stack className="cards-frame">
             {popularDishes.length !== 0 ? (
-              popularDishes.map((ele: Product) => {
-                const imagePath = `${serverApi}/${ele.productImages[0]}`;
+              popularDishes.map((product: Product) => {
+                const imagePath = `${serverApi}/${product.productImages[0]}`;
                 return (
-                  <CssVarsProvider key={ele._id}>
+                  <CssVarsProvider key={product._id}>
                     <Card className={"card"}>
                       <CardCover>
                         <img src={imagePath} alt="" />
@@ -54,7 +51,7 @@ export default function PopularDishes() {
                             textColor="#fff"
                             mb={1}
                           >
-                            {ele.productName}
+                            {product.productName}
                           </Typography>
                           <Typography
                             sx={{
@@ -64,7 +61,7 @@ export default function PopularDishes() {
                               display: "flex",
                             }}
                           >
-                            {ele.productViews}
+                            {product.productViews}
                             <VisibilityIcon
                               sx={{ fontSize: 25, marginLeft: "5px" }}
                             />
@@ -85,7 +82,7 @@ export default function PopularDishes() {
                           startDecorator={<DescriptionOutlinedIcon />}
                           textColor="neutral.300"
                         >
-                          {ele.productDesc}
+                          {product.productDesc}
                         </Typography>
                       </CardOverflow>
                     </Card>
@@ -101,3 +98,4 @@ export default function PopularDishes() {
     </div>
   );
 }
+                        
--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -5,23 +5,23 @@ import NewDishes from "./NewDishes";
 import Advertisement from "./Advertisement";
 import ActiveUsers from "./ActiveUsers";
 import Events from "./Events";
-import "../../../css/home.css";
-
 import { useDispatch } from "react-redux";
 import { Dispatch } from "@reduxjs/toolkit";
-import { setPopularDishes } from "./slice";
+import { setNewDishes, setPopularDishes } from "./slice";
 import { Product } from "../../../lib/types/product";
 import ProductService from "../../services/ProductService";
 import { ProductCollection } from "../../../lib/enums/product.enum";
+import "../../../css/home.css";
 
 /** REDUX SLICE & SELECTOR **/
 const actionDispatch = (dispatch: Dispatch) => ({
   setPopularDishes: (data: Product[]) => dispatch(setPopularDishes(data)), // => setPopularDishes commandasini setPopularDishes reduceri orqali hosil qilib oldik
+  setNewDishes: (data: Product[]) => dispatch(setNewDishes(data)),
 }); // setPopularDishes: commanda va reducer ni bir xil atadik va 1 chi kelgan commanda, 2 chi kelgan reducer
 
 
 export default function HomePage() {
-  const { setPopularDishes } = actionDispatch(useDispatch()); // function component ichida setPopularDishes ni caqirib qo'lga olyapmiz
+  const { setPopularDishes, setNewDishes } = actionDispatch(useDispatch()); // function component ichida setPopularDishes ni caqirib qo'lga olyapmiz
 
   // console.log(process.env.REACT_APP_API_URL);
 
@@ -44,6 +44,18 @@ export default function HomePage() {
         setPopularDishes(data); // qabul qilingan datani (redux Storage ga)setPopularDishes ga yuklaydi
       })
       .catch((err) => console.log(err));
+
+      product
+        .getProducts({
+          page: 1,
+          limit: 4,
+          order: "createdAt",  // eng oxiri qo'shilgan taomlar 
+          // productCollection: ProductCollection.DISH,
+        })
+        .then((data) => {
+          setNewDishes(data);
+        })
+        .catch((err) => console.log(err));
     // Slice: Data => Store (Slice mantig'i Backend dan kelgan Datani Redux Storage ga joylaydi )
   }, []);
   
