a32c97ee6d2577594c71f4ae3da0594e879c7a4c
Felix0308 <utkirbekm.0327@gmail.com>
2025-02-16 17:09:42 +0900

    fix: modify ChosenProduct component logic

--- src/app/services/MemberService.ts
+++ src/app/services/MemberService.ts
@@ -25,10 +25,12 @@ class MemberService {
     try {
       const url = `${this.path}/member/restaurant`;
       const result = await axios.get(url);
+      console.log("getRestaurant:", result);
 
-      return result.data;
+      const restaurant: Member = result.data;
+      return restaurant;
     } catch (err) {
-      console.log("Error, getTopUsers:", err);
+      console.log("Error, getRestaurant:", err);
       throw err;
     }
   }
--- src/app/services/ProductService.ts
+++ src/app/services/ProductService.ts
@@ -3,7 +3,8 @@ import { serverApi } from "../../lib/config";
 import { Product, ProductInquiry } from "../../lib/types/product";
 
 // ProductService Api => productlarga daxldor malumotlarni backenddan olish maqsadida qurildi
-class ProductService {  //=>  ProductService nomli class
+class ProductService {
+  //=>  ProductService nomli class
   private readonly path: string; // path nomli state, type string
 
   constructor() {
@@ -11,9 +12,10 @@ class ProductService {  //=>  ProductService nomli class
     this.path = serverApi; // path ni qiymatini serverApi orqali hosil qildik.(backendimiz linkini- http://localhost:3003  qo'yib beradi)
   }
 
-  public async getProducts(input: ProductInquiry): Promise<Product[]> {  // ProductService ni getProducts methodi hosil qildik
+  public async getProducts(input: ProductInquiry): Promise<Product[]> {
+    // ProductService ni getProducts methodi hosil qildik
     try {
-      let url = `${this.path}/product/all?order=${input.order}&page=${input.page}&limit=${input.limit}`; 
+      let url = `${this.path}/product/all?order=${input.order}&page=${input.page}&limit=${input.limit}`;
       // url => axios da ishlatish maqsadida
       if (input.productCollection)
         url += `&productCollection=${input.productCollection}`;
@@ -23,12 +25,24 @@ class ProductService {  //=>  ProductService nomli class
       console.log("getProducts:", result);
 
       return result.data;
-    } catch(err) {
+    } catch (err) {
       console.log("Error, getProduct:", err);
       throw err;
-      
     }
-  } 
+  }
+
+  public async getProduct(productId: string): Promise<Product> {
+    try {
+      const url = `${serverApi}/product/${productId}`;
+      const result = await axios.get(url, { withCredentials: true });
+      console.log("getProduct:", result);
+
+      return result.data;
+    } catch (err) {
+      console.log("Error, getProduct", err);
+      throw err;
+    }
+  }
 }
 
 export default ProductService;
